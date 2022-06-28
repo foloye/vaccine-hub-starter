de5da59da753f119c76a1ec83df8b15d2a7e9d98
foloye <foloye11@gmail.com>
2022-06-28 13:10:22 -0700

    step 4 done

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { PORT } = require("./config")
 
 const { BadRequestError, NotFoundError } = require("./utils/errors")
 
@@ -27,7 +28,6 @@ app.use((err, req,res, next) => {
 })
 
 
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`)
 } )
